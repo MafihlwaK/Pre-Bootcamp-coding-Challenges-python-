69c68164c9dcfb58467bd837e1aca51928114079
MafihlwaK <mafihlwak@yahoo.com>
2020-10-30 12:34:12 +0200

    task4

--- Task 4.py
+++ Task 4.py
@@ -1,10 +1,7 @@
 def my_number(no1, no2):
-    if no1 == 3 or no2 == 3:
-        return True
-    elif no2 + no1 == 3:
+    if no1 == 3 or no2 == 3 and "3" in str(no1 + no2) == 3:
         return True
     else :
         return False
     
-
-print(my_number(2, 21))
+print(my_number(12, 11))
