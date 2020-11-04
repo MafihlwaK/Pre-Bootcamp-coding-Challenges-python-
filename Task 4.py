d09e74bcf485d190921c1ad41cb1d93f4338a781
MafihlwaK <mafihlwak@yahoo.com>
2020-11-04 14:19:00 +0200

    changes

--- Task 4.py
+++ Task 4.py
@@ -1,5 +1,5 @@
 def my_number(no1, no2):
-    if no1 == 3 or no2 == 3 and "3" in str(no1 + no2) == 3:
+    if "3" in str(no1 + no2) == 3 and no1 == 3 or no2 == 3:
         return True
     else :
         return False
