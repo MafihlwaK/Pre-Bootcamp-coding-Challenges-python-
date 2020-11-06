f332607bb424098ab6d4759182ba3e5174ba1696
MafihlwaK <mafihlwak@yahoo.com>
2020-11-06 14:28:14 +0200

    task4

--- Task 4.py
+++ Task 4.py
@@ -1,7 +1,7 @@
 def my_number(no1, no2):
-    if "3" in str(no1 + no2) == 3 and no1 == 3 or no2 == 3:
+    if "3" in str(no1 + no2) == 3 and (no1 == 3 or no2 == 3):
         return True
     else :
         return False
     
-print(my_number(12, 11))
+print(my_number(2, 11))
